824f1e3c443411f88bef0f7f6639c496ac91fb0d
Danielle Martinoli <daniechris5@gmail.com>
2019-05-03 18:04:23 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,16 +1,16 @@
 def loop_iterator(number_of_times)
   counter = 0
-    loop do
+  loop do
       puts "Welcome to Flatiron School's Web Development Course!"
       break if counter == 7
       counter +=
+      end
   end
-end
 
 def times_iterator(number_of_times)
   7.times do
-  puts "Welcome to Flatiron School's Web Development Course!"
-end
+   puts "Welcome to Flatiron School's Web Development Course!"
+  end
 
 def while_iterator(number_of_times)
   counter = 1
@@ -18,7 +18,6 @@ def while_iterator(number_of_times)
   puts "Welcome to Flatiron School's Web Development Course!"
   break if counter == 3
   counter +=
-end
 end
 
 def until_iterator(number_of_times)
